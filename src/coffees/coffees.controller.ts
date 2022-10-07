0527ee87641fe0f2c890cccd9b08b0ddcf4f7bf8
Isaac Colls <colls_isaac@hotmail.com>
2022-10-06 22:42:35 -0300

    feat: creating a basic controller

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('coffees')
+export class CoffeesController {
+  @Get('flavors')
+  findAll() {
+    return 'this action returns all coffees ☕';
+  }
+}
