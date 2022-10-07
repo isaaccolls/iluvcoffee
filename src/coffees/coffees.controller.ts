32bcc841a369b6b5eb25ad631744b24df2787d2a
Isaac Colls <colls_isaac@hotmail.com>
2022-10-06 23:09:30 -0300

    feat: implementing pseudo pagination with query parameters

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -6,13 +6,15 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll() {
-    return 'this action returns all coffees ☕';
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
+    return `this action returns all coffees ☕. limit: ${limit}, offset: ${offset}`;
   }
 
   @Get(':id')
