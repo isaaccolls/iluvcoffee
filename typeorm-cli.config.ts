0f4495e1838a7775fb94149ca26ccf6cb7189ef2
Isaac Colls <colls_isaac@hotmail.com>
2022-10-11 18:10:34 -0300

    feat: setting up migrations

---
+++ typeorm-cli.config.ts
@@ -0,0 +1,13 @@
+import { CoffeeRefactor1665521995285 } from 'src/migrations/1665521995285-CoffeeRefactor';
+import { DataSource } from 'typeorm';
+
+export default new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'pass123',
+  database: 'postgres',
+  entities: [],
+  migrations: [CoffeeRefactor1665521995285],
+});
