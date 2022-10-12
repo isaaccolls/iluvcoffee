218556a9aae3a90eda2944c569f9723526da6a99
Isaac Colls <colls_isaac@hotmail.com>
2022-10-12 17:31:19 -0300

    feat: asynchronously configure dynamic modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,28 +7,25 @@ import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
 import appConfig from './config/app.config';
-// import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      // validationSchema: Joi.object({
-      //   DATABASE_HOST: Joi.required(),
-      //   DATABASE_PORT: Joi.number().default(5432),
-      // }),
-      load: [appConfig],
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        // models will be loaded automatically
+        autoLoadEntities: true,
+        // entities will be synced with the database(recommended: disable in prod)
+        synchronize: true,
+      }),
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      // models will be loaded automatically
-      autoLoadEntities: true,
-      // entities will be synced with the database(recommended: disable in prod)
-      synchronize: true,
+    ConfigModule.forRoot({
+      load: [appConfig],
     }),
     CoffeesModule,
     CoffeeRatingModule,
