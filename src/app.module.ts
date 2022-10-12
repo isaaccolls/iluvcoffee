1b1b125f85b234338d195b5c950e0609bf08e18e
Isaac Colls <colls_isaac@hotmail.com>
2022-10-12 11:10:51 -0300

    feat: coffee rating module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { CoffeesModule } from './coffees/coffees.module';
       synchronize: true,
     }),
     CoffeesModule,
+    CoffeeRatingModule,
   ],
   controllers: [AppController],
   providers: [AppService],
