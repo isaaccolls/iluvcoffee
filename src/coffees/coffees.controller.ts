486b0cc26397ffca7819727642cc2d715ab45ab4
Isaac Colls <colls_isaac@hotmail.com>
2022-11-02 18:48:04 -0300

    feat:    Adding Example Responses

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,6 +8,7 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { ApiForbiddenResponse } from '@nestjs/swagger';
 import { Protocol } from 'src/common/decorators/protocol.decorator';
 import { Public } from 'src/common/decorators/public.decorator';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
@@ -20,6 +21,7 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
+  @ApiForbiddenResponse({ description: 'forbidden' })
   @Public()
   @Get()
   async findAll(
