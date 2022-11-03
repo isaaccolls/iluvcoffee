66d3a06e8760a008c22a3370e133b4e3ee3193ff
Isaac Colls <colls_isaac@hotmail.com>
2022-11-03 18:43:42 -0300

    feat: Adding Unit Tests

--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,7 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConfigModule } from '@nestjs/config';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
+import { NotFoundException } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
 import {
   ASYNC_COFFEE_BRANDS,
   COFFEE_BRANDS,
@@ -12,16 +13,29 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import coffeesConfig from './config/coffees.config';
 
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+const createMockRepository = <T = any>(): MockRepository<T> => ({
+  findOne: jest.fn(),
+  create: jest.fn(),
+});
+
 describe('CoffeesService', () => {
   let service: CoffeesService;
+  let coffeeRepository: MockRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         CoffeesService,
         { provide: DataSource, useValue: {} },
-        { provide: getRepositoryToken(Flavor), useValue: {} }, // 👈
-        { provide: getRepositoryToken(Coffee), useValue: {} }, // 👈
+        {
+          provide: getRepositoryToken(Flavor),
+          useValue: createMockRepository(),
+        }, // 👈
+        {
+          provide: getRepositoryToken(Coffee),
+          useValue: createMockRepository(),
+        }, // 👈
         // this should not be necessary
         { provide: COFFEE_BRANDS, useValue: [] },
         { provide: OTHER_COFFEE_BRANDS, useValue: [] },
@@ -32,9 +46,35 @@ describe('CoffeesService', () => {
     }).compile();
 
     service = module.get<CoffeesService>(CoffeesService);
+    coffeeRepository = module.get<MockRepository>(getRepositoryToken(Coffee));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    describe('when coffee with ID exists', () => {
+      it('should return the coffee object', async () => {
+        const coffeeId = '1';
+        const expectedCoffee = {};
+        coffeeRepository.findOne.mockReturnValue(expectedCoffee);
+        const coffee = await service.findOne(coffeeId);
+        expect(coffee).toEqual(expectedCoffee);
+      });
+    });
+    describe('otherwise', () => {
+      it('should throw the "NotFoundException"', async () => {
+        const coffeeId = '1';
+        coffeeRepository.findOne.mockReturnValue(undefined);
+        try {
+          await service.findOne(coffeeId);
+          expect(false).toBeTruthy(); // we should never hit this line
+        } catch (err) {
+          expect(err).toBeInstanceOf(NotFoundException);
+          expect(err.message).toEqual(`coffee #${coffeeId} not found`);
+        }
+      });
+    });
   });
 });
